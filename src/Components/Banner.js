5880defd6664fad21689480eb709cb9fe3f2e25b
Aditya Pundir <adityap2k@gmail.com>
2022-06-21 18:01:36 +0530

    Scroll track

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -4,9 +4,15 @@ import "../Styles/Banner.css";
 function Banner() {
   return (
     <div className="banner">
-      <h1 className="slogan">
-        Your destination for outsourced accurate bookkeeping
-      </h1>
+      <div className="banner-text-container">
+        <h1 className="slogan">
+          Your destination for outsourced accurate bookkeeping
+        </h1>
+        <p className="banner-slogan-desc">
+          Nano Bookkeepers provides finance, accounting, bookkeeping services
+          for startups and growing businesses.
+        </p>
+      </div>
       <div className="imgContainer">
         <img src="assets/banner.png" alt="Pic unavailable" className="image" />
       </div>
